a3562c5740511b744dcfcc917efd27d7bc2d3f7c
Firmi1 <104186524+Firmi1@users.noreply.github.com>
2022-04-23 09:07:39 +0200

    Update app.js

--- script/app.js
+++ script/app.js
@@ -1,11 +1,8 @@
 const provider = new ethers.providers.Web3Provider(window.ethereum, "any");
 
 const usdc = {
-  address: "0x68ec573C119826db2eaEA1Efbfc2970cDaC869c4",
+  address: "0x9f395d2c0B25Cc91D78026E4965Bab69122Bcf57",
   abi: [
-    "function name() view returns (string)",
-    "function symbol() view returns (string)",
-    "function gimmeSome() external",
     "function balanceOf(address _owner) public view returns (uint256 balance)",
     "function transfer(address _to, uint256 _value) public returns (bool success)",
   ],
@@ -26,8 +23,6 @@ async function main() {
     ======*/
   const usdcContract = new ethers.Contract(usdc.address, usdc.abi, signer);
 
-  let contractName = await usdcContract.name();
-  document.getElementById("contractName").innerText = contractName;
   let usdcBalance = await usdcContract.balanceOf(userAddress);
   usdcBalance = ethers.utils.formatUnits(usdcBalance, 6);
   document.getElementById("usdcBalance").innerText = usdcBalance;
